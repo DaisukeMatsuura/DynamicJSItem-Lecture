606e08b0fbe4bf873a00199fc7e20f23c167a4fe
DaisukeMatsuura <mail@brightful.jp>
2024-05-02 08:51:26 +0900

    addItem で input とdelete button を追加する処理を追加

--- main.js
+++ main.js
@@ -5,9 +5,28 @@ console.log("main.js が読み込まれました!");
 function addItem() {
   console.log("addItem 実行!");
   // ここに追加ボタンを押した時の処理を書く
+  const itemInput = document.getElementById("itemInputs");
+
+  const newItemDiv = document.createElement("div");
+
+  const newItemInput = document.createElement("input");
+  newItemInput.type = "text";
+  newItemInput.className = "item";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "削除";
+  deleteButton.type = "button";
+  deleteButton.addEventListener("click", function() {
+    deleteItem(this);
+  });
+
+  newItemDiv.appendChild(newItemInput);
+  newItemDiv.appendChild(deleteButton);
+
+  itemInput.appendChild(newItemDiv);
 }
 
-function deleteItem() {
+function deleteItem(button) {
   console.log("deleteItem 実行!");
 
   // ここに削除ボタンを押した時の処理を書く
