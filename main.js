79fced3d95470622afc3590be7b34ad3a5f1ad83
DaisukeMatsuura <mail@brightful.jp>
2024-05-02 15:06:16 +0900

    コメントを追加

--- main.js
+++ main.js
@@ -4,15 +4,21 @@ console.log("main.js が読み込まれました!");
 
 function addItem() {
   console.log("addItem 実行!");
-  // ここに追加ボタンを押した時の処理を書く
+
+  // アイテム行を追加するDivを取得する
   const itemInput = document.getElementById("itemInputs");
 
+  // 挿入するDivを作成する
   const newItemDiv = document.createElement("div");
 
+  // 挿入するInputを作成し、type・name をそれぞれ指定する
   const newItemInput = document.createElement("input");
   newItemInput.type = "text";
   newItemInput.name = "item";
 
+  // 追加したInputを削除するためのボタンを作成し、
+  // textContent・typeを指定する
+  // また addEventListener でクリックイベントを追加する
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "削除";
   deleteButton.type = "button";
@@ -20,9 +26,11 @@ function addItem() {
     deleteItem(this);
   });
 
+  // 挿入するDivの中に Input と Button を追加
   newItemDiv.appendChild(newItemInput);
   newItemDiv.appendChild(deleteButton);
 
+  // 対象のDivに作成したDivを追加
   itemInput.appendChild(newItemDiv);
 }
 
